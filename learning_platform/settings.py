21688120d4705d82a121e7a83c1838e05b0c5ec4
S.M. Rifat <55460433+rifatblack@users.noreply.github.com>
2024-11-20 23:52:11 +0600

    update

--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-1(bmuwjqv_09#)+iw&frk2ml2vy-#uyq&uxoa4vd+on$#07=16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
